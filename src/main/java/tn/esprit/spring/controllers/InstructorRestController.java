655b4d8762e54ec2548442a2cb958f21c93d7b35
nidhal66 <80532935+nidhal66@users.noreply.github.com>
2024-10-11 21:00:21 +0100

    Update InstructorRestController.java

--- src/main/java/tn/esprit/spring/controllers/InstructorRestController.java
+++ src/main/java/tn/esprit/spring/controllers/InstructorRestController.java
@@ -1,5 +1,7 @@
 package tn.esprit.spring.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -16,28 +18,73 @@ import java.util.List;
 public class InstructorRestController {
 
     private final IInstructorServices instructorServices;
+    private static final Logger logger = LogManager.getLogger(MyController.class);
 
     @Operation(description = "Add Instructor")
     @PostMapping("/add")
     public Instructor addInstructor(@RequestBody Instructor instructor){
+           logger.info("addInstructorMethod was called.");
+
+        try {
+            // Simulate some processing
+            logger.debug("Processing data...");
+            // Your processing logic here
         return  instructorServices.addInstructor(instructor);
-    }
+        } catch (Exception e) {
+            logger.error("An error occurred while processing data: ", e);
+            return "Error!";
+        }
+    }    
+    
     @Operation(description = "Add Instructor and Assign To Course")
     @PutMapping("/addAndAssignToCourse/{numCourse}")
     public Instructor addAndAssignToInstructor(@RequestBody Instructor instructor, @PathVariable("numCourse")Long numCourse){
+              logger.info("addAndAssignToInstructorMethod was called.");
+
+        try {
+            // Simulate some processing
+            logger.debug("Processing data...");
+            // Your processing logic here
         return  instructorServices.addInstructorAndAssignToCourse(instructor,numCourse);
-    }
+        } catch (Exception e) {
+            logger.error("An error occurred while processing data: ", e);
+            return "Error!";
+        }
+    }    
+    
     @Operation(description = "Retrieve all Instructors")
     @GetMapping("/all")
     public List<Instructor> getAllInstructors(){
+                    logger.info("getAllInstructorMethod was called.");
+
+        try {
+            // Simulate some processing
+            logger.debug("Processing data...");
+            // Your processing logic here
         return instructorServices.retrieveAllInstructors();
-    }
+        } catch (Exception e) {
+            logger.error("An error occurred while processing data: ", e);
+            return "Error!";
+        }
+    }    
+    
 
     @Operation(description = "Update Instructor ")
     @PutMapping("/update")
     public Instructor updateInstructor(@RequestBody Instructor Instructor){
+                  logger.info("updateInstructorMethod was called.");
+
+        try {
+            // Simulate some processing
+            logger.debug("Processing data...");
+            // Your processing logic here
         return  instructorServices.updateInstructor(Instructor);
-    }
+        } catch (Exception e) {
+            logger.error("An error occurred while processing data: ", e);
+            return "Error!";
+        }
+    }    
+    
 
     @Operation(description = "Retrieve Instructor by Id")
     @GetMapping("/get/{id-instructor}")
