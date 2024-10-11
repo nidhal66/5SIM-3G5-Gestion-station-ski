6ecd7e73774ad52b6d5a10683adcbf4b8f188710
nidhal66 <80532935+nidhal66@users.noreply.github.com>
2024-10-11 21:40:12 +0100

    Update CourseRestControllerTest.java

--- src/test/java/tn/esprit/spring/CourseRestControllerTest.java
+++ src/test/java/tn/esprit/spring/CourseRestControllerTest.java
@@ -14,7 +14,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import tn.esprit.spring.controllers.CourseRestController;
-import tn.esprit.spring.entities.Course;
+import tn.esprit.spring.entities.TypeCourse;
+import tn.esprit.spring.entities.Support;
+
 import tn.esprit.spring.services.ICourseServices;
 
 import java.util.ArrayList;
