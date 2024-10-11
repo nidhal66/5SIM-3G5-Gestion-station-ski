7b43b816e67d470f683245c56d62f1530b3cbf86
nidhal66 <80532935+nidhal66@users.noreply.github.com>
2024-10-11 21:37:58 +0100

    Update Course.java

--- src/main/java/tn/esprit/spring/entities/Course.java
+++ src/main/java/tn/esprit/spring/entities/Course.java
@@ -34,22 +34,5 @@ public class Course implements Serializable {
 	@JsonIgnore
 	@OneToMany(mappedBy= "course")
 	Set<Registration> registrations;
-// Getter and Setter for id
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    // Getter and Setter for name
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
